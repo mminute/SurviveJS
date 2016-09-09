e0ef22e668696ff3ddf4b7c414d3e6b93d70e39a
mminute <masondjennings@gmail.com>
2016-09-08 21:13:48 -0700

    Fix indentations

--- app/component.js
+++ app/component.js
@@ -1,5 +1,5 @@
 module.exports = function() {
-  var element = document.createElement('h1');
-  element.innerHTML = 'hello world';
-  return element;
+    var element = document.createElement('h1');
+    element.innerHTML = 'hello world';
+    return element;
 };
