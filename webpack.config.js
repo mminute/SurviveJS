2d578cfd1584aad8ad63cd6e79fcee8099bd385e
mminute <masondjennings@gmail.com>
2016-09-11 21:50:29 -0700

    Adds hot reloading for React components

--- webpack.config.js
+++ webpack.config.js
@@ -12,6 +12,8 @@ const PATHS = {
     build: path.join(__dirname, 'build')
 };
 
+process.env.BABEL_ENV = TARGET;
+
 const common = {
     // tell it where to get the files
     // Entry accepts a path or an object of entries.  We'll be using
