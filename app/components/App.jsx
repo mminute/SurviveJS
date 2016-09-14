304f1108270d39180988943bc8ca2df64514d8c3
mminute <masondjennings@gmail.com>
2016-09-13 21:27:45 -0700

    Adds constructor and state to App

--- app/components/App.jsx
+++ app/components/App.jsx
@@ -2,26 +2,32 @@ import React, {Component} from 'react';
 import uuid from 'node-uuid';
 
 export default class App extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            notes : [
+                {
+                    id: uuid.v4(),
+                    task: 'Learn Webpack'
+                },
+                {
+                    id: uuid.v4(),
+                    task: 'Learn React'
+                },
+                {
+                    id: uuid.v4(),
+                    task: 'Do Laundry'
+                }
+            ]
+        }
+    }
     render() {
-        const notes = [
-            {
-                id: uuid.v4(),
-                task: 'Learn Webpack'
-            },
-            {
-                id: uuid.v4(),
-                task: 'Learn React'
-            },
-            {
-                id: uuid.v4(),
-                task: 'Do Laundry'
-            }
-        ];
         return (
             <div>
                 <ul>
                     {
-                        notes.map(note =>
+                        this.state.notes.map(note =>
                             <li key={note.id}>
                                 {note.task}
                             </li>
