92c6c6c1882c6264c0f3addf1cc75e0e7243b84c
mminute <masondjennings@gmail.com>
2016-09-27 21:54:07 -0700

    Sets up skeleton for NoteStore

---
+++ app/stores/NoteStore.js
@@ -0,0 +1,17 @@
+import uuid from 'node-uuid';
+import alt from '../libs/alt';
+import NoteActions from '../actions/NoteActions';
+
+class NoteStore {
+  constructor() {
+    this.bindActions(NoteActions);
+
+    this.notes = [];
+  }
+
+  create(note) {}
+
+  update(updatedNote) {}
+
+  delete(id) {}
+}
