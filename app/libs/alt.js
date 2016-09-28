b5593deeee5c0cdd734a618b1310afb4b4cb50cf
mminute <masondjennings@gmail.com>
2016-09-27 21:42:59 -0700

    Sets up Alt instance

---
+++ app/libs/alt.js
@@ -0,0 +1,8 @@
+import Alt from 'alt';
+// import chromeDebug from 'alt-utils/lib/chromeDebug';
+// for use with Chrome plugin alt-devtool
+
+const alt = new Alt();
+// chromeDebug(alt);
+
+export default alt;
