5e7f8505a69cdc1109041e79139cd3fb7ec5c5a1
mminute <masondjennings@gmail.com>
2016-09-29 20:56:12 -0700

    Creates Redux Store and Actions

--- app/components/App.jsx
+++ app/components/App.jsx
@@ -1,44 +1,38 @@
 import React, {Component} from 'react';
-import uuid from 'node-uuid';
 import Notes from './Notes';
+import NoteActions from '../actions/NoteActions';
+import NoteStore from '../stores/NoteStore';
 
 export default class App extends Component {
     constructor(props) {
         super(props);
 
-        this.state = {
-            notes: [
-                {
-                    id: uuid.v4(),
-                    task: 'Learn Webpack'
-                },
-                {
-                    id: uuid.v4(),
-                    task: 'Learn React'
-                },
-                {
-                    id: uuid.v4(),
-                    task: 'Do Laundry'
-                }
-            ]
-        };
+        this.state = NoteStore.getState();
+    }
+
+    componentDidMount() {
+        NoteStore.listen(this.storeChanged);
+    }
+
+    componentWillUnmount() {
+        NoteStore.unlisten(this.storeChanged);
+    }
+
+    storeChanged = (state) => {
+        this.setState(state);
     }
 
     addNote = () => {
-        const notes = [...this.state.notes, {id: uuid.v4(), task: 'newTask'}];
-
-        this.setState({notes});
+        NoteActions.create({task: 'newTask'});
     }
 
     handleDeleteNote = (id, e) => {
         // Avoid bubbling to edit
         if (e) {
-            e.stopPropagation();
+          e.stopPropagation();
         }
 
-        const notes = this.state.notes.filter(note => note.id !== id);
-
-        this.setState({notes});
+        NoteActions.delete(id);
     }
 
     handleEditNote = (id, task) => {
@@ -47,15 +41,7 @@ export default class App extends Component {
             return;
         }
 
-        const notes = this.state.notes.map(note => {
-            if (note.id === id && task) {
-                note.task = task;
-            }
-
-            return note;
-        });
-
-        this.setState({notes});
+        NoteActions.update({id, task})
     }
 
     render() {
--- app/stores/NoteStore.js
+++ app/stores/NoteStore.js
@@ -3,40 +3,54 @@ import alt from '../libs/alt';
 import NoteActions from '../actions/NoteActions';
 
 class NoteStore {
-  constructor() {
-    this.bindActions(NoteActions);
+    constructor() {
+        this.bindActions(NoteActions);
 
-    this.notes = [];
-  }
+        this.notes =
+            [
+                {
+                    id: uuid.v4(),
+                    task: 'Learn Webpack'
+                },
+                {
+                    id: uuid.v4(),
+                    task: 'Learn React'
+                },
+                {
+                    id: uuid.v4(),
+                    task: 'Do Laundry'
+                }
+            ];
+    }
 
-  create(note) {
-    note.id = uuid.v4();
+    create(note) {
+        note.id = uuid.v4();
 
-    const notes = [...this.notes, note];
+        const notes = [...this.notes, note];
 
-    this.setState({notes})
-  }
+        this.setState({notes})
+    }
 
-  update(updatedNote) {
-    const notes = this.notes.map(note => {
-      if (note.id === updatedNote.id) {
-        // Object.assign is used to patch the note data here.
-        // It mutates the target (first parameter).
-        // In order to avoid this we use an empty object as the target
-        // and apply data to it.  You can pass as many args as desired
-        return Object.assign({}, note, updatedNote);
-      }
-      return note;
-    });
+    update(updatedNote) {
+        const notes = this.notes.map(note => {
+            if (note.id === updatedNote.id) {
+                // Object.assign is used to patch the note data here.
+                // It mutates the target (first parameter).
+                // In order to avoid this we use an empty object as the target
+                // and apply data to it.  You can pass as many args as desired
+                return Object.assign({}, note, updatedNote);
+            }
+            return note;
+        });
 
-    this.setState({notes});
-  }
+        this.setState({notes});
+    }
 
-  delete(id) {
-    this.setState({
-      notes: this.notes.filter(note => note.id !== id)
-    });
-  }
+    delete(id) {
+        this.setState({
+            notes: this.notes.filter(note => note.id !== id)
+        });
+    }
 }
 
 export default alt.createStore(NoteStore, 'NoteStore');
